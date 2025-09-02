89cc2328fe353f84a06872d2577a74365d576e90
admin <deepakverma726800@gmail.com>
2025-09-02 10:48:46 +0530

    ok

---
+++ basic/OperatorClass.java
@@ -0,0 +1,28 @@
+package com.rays.basic;
+
+public class OperatorClass {
+	package com.rays.basic;
+
+	public class Operators {
+		
+	}
+
+		// = Assignment operator
+
+		/**
+		 * Assignment operator (`=`) is used to assign the value of the right-hand
+		 * operand to the left-hand operand. For example:
+		 * 
+		 * <pre>
+		 * int x = 10; // assigns the value 10 to variable x
+		 * </pre>
+		 */
+
+		// == Equal operator
+
+		/**
+		 * Equality operator (`==`) checks if two operands are equal. It returns `true`
+		 * if they are equal, otherwise `false`. For example:
+
+
+}
