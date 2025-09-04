171c087012e61528ec015e320bc5e7f88fdb8e2c
admin <deepakverma726800@gmail.com>
2025-09-04 14:06:44 +0530

    overriding

---
+++ java-OOP/src/com/rays/overriding/Shape.java
@@ -0,0 +1,25 @@
+package com.rays.oopInheritance.overriding;
+
+public class Shape {
+	
+	protected String color;
+	protected int borderWidth;
+	
+	public String getColor() {
+		return this.color;
+	}
+	public void setColor(String color) {
+		this.color=color;
+	}
+	public int getBorderWidth() {
+		return this.borderWidth;
+	}
+	public void setBorderWidth(int borderWidth) {
+		this.borderWidth=borderWidth;
+				
+	}
+	public double area() {
+		return 0.0;
+	}
+
+}
