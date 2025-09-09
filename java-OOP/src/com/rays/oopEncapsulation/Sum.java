5494c336b2bb7844e0836d36077ab5819513fef7
admin <deepakverma726800@gmail.com>
2025-09-09 13:36:37 +0530

    done

---
+++ java-OOP/src/com/rays/oopEncapsulation/Sum.java
@@ -0,0 +1,17 @@
+package com.rays.oopEncapsulation;
+
+public class Sum {
+	
+	public void sum(int i,int j) {
+		System.out.println("sum of two int number is :"+(i+j));
+	}
+	public void sum(int i ,int j,int k) {
+		System.out.println("sum of three int no :"+(i+j+k));
+	}
+	public void sum(double i,double j) {
+		System.out.println("sum of two double no :"+(i+j));
+		
+	}
+	
+
+}
---
+++ java-OOP/src/com/rays/oopEncapsulation/TestSum.java
@@ -0,0 +1,14 @@
+package com.rays.oopEncapsulation;
+
+public class TestSum {
+	public static void main(String[] args) {
+
+		Sum s = new Sum();
+
+		s.sum(1, 2);
+		s.sum(2.6, 10.0);
+		s.sum(1,2 , 3);
+		
+
+	}
+}
