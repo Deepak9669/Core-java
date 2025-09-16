8530cd0244ad2be3564f3eecb62a78b6628f3945
Deepak9669 <deepakverma726800@gmail.com>
2025-09-16 14:38:44 +0530

    done

--- java-collection/src/com/rays/collection/TestIterator.java
+++ java-collection/src/com/rays/collection/TestIterator.java
@@ -1,8 +1,38 @@
 package com.rays.collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 public class TestIterator {
 	public static void main(String[] args) {
 		
+		ArrayList list=new ArrayList();
+		
+		list.add("Ram");
+		list.add("shyam");
+		list.add("ritesh");
+		list.add("rishabh");
+		list.add("deepak");
+		
+		
+		System.out.println("list :"+ list);
+		
+		System.out.println("_______for each loop__________");
+		
+		for(Object o : list) {
+			System.out.println(o);
+		}
+		System.out.println("________iterator__________");
+		
+	   Iterator it=list.iterator();
+	   while(it.hasNext()) {
+		  Object o=it.next();
+	   System.out.println(o);
+//	   it.remove();
+	   }
+	   System.out.println("_______for each methods___________");
+	   list.forEach(System.out::println);
 	}
 
 }
